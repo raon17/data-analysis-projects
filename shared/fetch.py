59f0138760327d7258068c60290fb310b7035e83
William Kim <willkim0117@gmail.com>
2026-03-04 21:30:40 +1100

    fetch.py updated

--- shared/fetch.py
+++ shared/fetch.py
@@ -1,6 +1,13 @@
+"""
+Generic data fetching functions used across all projects
+Each project-specific API will get its own function here and return raw data 
+"""
+
+import os
 import requests
+from dotenv import load_dotenv
 
 def get_steam_data(url):
     response = requests.get(url)
-    response.raise_for_status()  # throws error if request failed
+    response.raise_for_status()
     return response.json()
