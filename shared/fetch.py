946605603ccacf488fd8f13d48c1c1156452bc69
William Kim <willkim0117@gmail.com>
2026-03-04 22:36:13 +1100

    Fetch.py updated

--- shared/fetch.py
+++ shared/fetch.py
@@ -1,13 +1,53 @@
-"""
-Generic data fetching functions used across all projects
-Each project-specific API will get its own function here and return raw data 
-"""
+#Generic data fetching functions used across all projects
+#Each project-specific API will get its own function here and return raw data 
 
-import os
-import requests
-from dotenv import load_dotenv
+import os 
+import requests 
+from dotenv import load_dotenv 
 
-def get_steam_data(url):
-    response = requests.get(url)
-    response.raise_for_status()
+def get_json(url: str, params: dict = None) -> dict:
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # Raise an error for bad status codes ()
     return response.json()
+
+def get_json_list(url: str, params: dict = None) -> list:
+    data = get_json(url, params)
+    if isinstance(data, list):
+        return data
+    raise ValueError(f"Expected a list but data returned is {type(data)}")
+
+#Used for fetching stock data from Alpha Vantage API, used in Project Crypto-Analysis
+def get_alpha_vantage_key() -> str:
+    key = os.getenv("ALPHA_VANTAGE_KEY")
+    if not key:
+        raise EnvironmentError(
+            "Alpha Vantage API key not found.\n"
+        )
+    return key
+
+#Fetch daily OHLCV data for a crypto coin from Alpha Vantage API, used in Project Crypto-Analysis
+def get_crypto_daily(symbol: str, market: str = "USD") -> dict:
+    url = "https://www.alphavantage.co/query"
+    params = {
+        "function":"DIGITAL_CURRENCY_DAILY",
+        "symbol":symbol,
+        "market":market,
+        "apikey":get_alpha_vantage_key(),
+        "outputsize":"full",
+    }
+
+    data = get_json(url, params)
+
+    # Error handling for API responses
+    if "Error Message" in data:
+        raise ValueError(f"Error {symbol}: {data['Error Message']}")
+
+    if "Note" in data:
+        raise RuntimeError(
+            f"Limit hit for {symbol}.\n, free tier allows 25 requests/day. Wait and try again.")
+
+    if "Information" in data:
+        raise RuntimeError(
+            f"Information for {symbol}: {data['Information']}"
+        )
+    return data
